22d43a635a3d77cba41664697a24a5f9123c9fbe
Joe Njarathadathil <joseph.njarathadathil@gmail.com>
2016-03-28 15:39:01 -0500

    bootstrap and seeded db

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160328174308) do
+ActiveRecord::Schema.define(version: 20160328174719) do
 
   create_table "owners", force: :cascade do |t|
+    t.string   "email"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Restaurant.create!([{
+    name: "Chipotle",
+    address: "Chicago, IL",
+    description: "asdfasga",
+    phone_number: "1234567890"
+  },
+  {
+    name: "Mcdonalds",
+    address: "Chicago, IL",
+    description: "asdfg",
+    phone_number: "1234567890"     
+  },
+  {
+    name: "Freshii",
+    address: "Chicago, IL",
+    description: "asdfsadfasd",
+    phone_number: "1234567890"
+  }
+])
+print "created #{Restaurant.count} Restaurants"
+
+Owner.create!([{
+    email: "test@gmail.com",
+    first_name: "Joe",
+    last_name: "N"
+  },
+  {
+    email: "test1@gmail.com",
+    first_name: "Viktor",
+    last_name: "S"
+  },
+  {
+    email: "test2@gmail.com",
+    first_name: "Jiha",
+    last_name: "H"
+  }
+])
