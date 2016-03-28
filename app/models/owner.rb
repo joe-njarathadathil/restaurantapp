d31abdeb5169b7392b7a329a8fa1875c995a420f
Joe Njarathadathil <joseph.njarathadathil@gmail.com>
2016-03-28 12:26:40 -0500

    navbar

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,5 +1,11 @@
 class Owner < ActiveRecord::Base
 	has_many :restaurant_owners
   has_many :restaurants, through: :restaurant_owners
+  
+  validates :first_name, presence: true
+  validates :last_name, presence: true 
+  validates :email, presence: true, uniqueness: true, 
+    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, message: "is not a valid email"}
+
 
 end
